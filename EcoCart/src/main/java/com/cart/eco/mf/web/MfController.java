f228838cbebf6963f9fd02e57f5911898b2062fc
ramier999 <ramier999@gmail.com>
2022-11-14 17:08:53 +0900

    commit

---
+++ EcoCart/src/main/java/com/cart/eco/mf/web/MfController.java
@@ -0,0 +1,13 @@
+package com.cart.eco.mf.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MfController {
+	
+	@GetMapping("/mfPlanList")
+	public String mfPlanList() {
+		return "manufacture/mfPlanList";
+	}
+}
