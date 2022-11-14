ef0527a42cf2241c62713969893f63cc4c2b4687
kim <lldarkunitll@gmail.com>
2022-11-14 12:54:56 +0900

    first one

---
+++ EcoCart/src/main/java/com/cart/eco/EcoCartApplication.java
@@ -0,0 +1,18 @@
+package com.cart.eco;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.cart.eco.**.mapper")
+public class EcoCartApplication {
+	
+	public static final Logger log = LoggerFactory.getLogger(EcoCartApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(EcoCartApplication.class, args);
+	}
+
+}
