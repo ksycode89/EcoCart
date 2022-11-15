21226c73deb193a59845aa3e34a11c73c5d9a337
edc1022 <sechswkd@hanmail.net>
2022-11-15 18:55:33 +0900

    수정작업

--- EcoCart/src/main/java/com/cart/eco/common/web/CommonController.java
+++ EcoCart/src/main/java/com/cart/eco/common/web/CommonController.java
@@ -2,7 +2,7 @@ package com.cart.eco.common.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller()
 public class CommonController {
