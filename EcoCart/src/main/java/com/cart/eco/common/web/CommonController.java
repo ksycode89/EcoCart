36d8b0aeed72af7b3d8a6b63bb1e91e3c4eb93ba
edc1022 <sechswkd@hanmail.net>
2022-11-15 14:35:31 +0900

    사원관리페이지 컨트롤러

--- EcoCart/src/main/java/com/cart/eco/common/web/CommonController.java
+++ EcoCart/src/main/java/com/cart/eco/common/web/CommonController.java
@@ -2,6 +2,7 @@ package com.cart.eco.common.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller("layout")
 public class CommonController {
@@ -12,6 +13,11 @@ public class CommonController {
 		return "common/login.html";
 	}
 	
-	
+	// 사원관리 페이지.
+	@GetMapping("/empList")
+	public String empForm() {
+		
+		return "common/employees.html";
+	}
 	
 }
