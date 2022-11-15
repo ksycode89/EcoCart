bd389e8f0fd9a004fe0162d672fec3de7e80ea95
정민선 <@DESKTOP-2O2PTAH>
2022-11-15 14:47:25 +0900

    11/15중간

--- EcoCart/src/main/java/com/cart/eco/open/web/EstimateController.java
+++ EcoCart/src/main/java/com/cart/eco/open/web/EstimateController.java
@@ -7,15 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class EstimateController {
 	
-	//견적서 조회(승인X)
-	@GetMapping("/list")
-	public String selectEstimateList() {
+	
+	@GetMapping("/estimate")
+	public String EstimateList() {
 		return "open/estimate";
 	}
 	
-	//견적 번호 가져오기
-
+	@GetMapping("/order")
+	public String OrderList() {
+		return "open/order";
+	}
 	
 	
-	//견적서 등록	
+	
 }
