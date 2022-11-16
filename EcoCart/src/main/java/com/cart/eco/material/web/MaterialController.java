03feac4c58d251de67ac521bad6e83adb55661e9
Namsoont <nst6173@naver.com>
2022-11-16 17:39:49 +0900

    자재재고조회 불량조회 재고이동 재고저정 html 출고처리 출고현황 레이아웃 하나뺌

--- EcoCart/src/main/java/com/cart/eco/material/web/MaterialController.java
+++ EcoCart/src/main/java/com/cart/eco/material/web/MaterialController.java
@@ -5,36 +5,64 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MaterialController {
-	
+
 	@GetMapping("/materialList")
 	public String materialList() {
 		return "material/materialList";
 	}
-	
+
 	@GetMapping("/materialinsert")
 	public String materialInsert() {
 		return "material/materialinsert";
 	}
-	
+
 	@GetMapping("/materialreta")
 	public String materialReta() {
 		return "material/materialreta";
 	}
-	
+
 	@GetMapping("/Wearingtest")
 	public String WearingTest() {
 		return "material/Wearingtest";
 	}
+
 	@GetMapping("/Wearingprocess")
 	public String WearingProcess() {
 		return "material/Wearingprocess";
 	}
+
 	@GetMapping("/WearingList")
 	public String WearingList() {
 		return "material/WearingList";
 	}
+
 	@GetMapping("/release")
 	public String Release() {
 		return "material/release";
+	}
+
+	@GetMapping("/releaseList")
+	public String ReleaseList() {
+		return "material/releaseList";
+	}
+	
+	@GetMapping("/receivingList")
+	public String ReceivingList() {
+		return "material/receivingList";
+	}
+	
+	@GetMapping("/badproduct")
+	public String Badproduct() {
+		return "material/badproduct";
+	}
+	
+	@GetMapping("/matmove")
+	public String Matmove() {
+		return "material/matmove";
+	}
+	
+	@GetMapping("/matment")
+	public String Matment() {
+		return "material/matment";
 	}
 }
