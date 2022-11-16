efe9f86702c2a6d83ea450f6b12f3ed3467490e5
정민선 <@DESKTOP-2O2PTAH>
2022-11-16 09:18:09 +0900

    11/16 최종 주문취소, 출고요청,출고등록,출고처리,배송정보,재고현왕/bom재고

--- EcoCart/src/main/java/com/cart/eco/open/web/EstimateController.java
+++ EcoCart/src/main/java/com/cart/eco/open/web/EstimateController.java
@@ -7,16 +7,54 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class EstimateController {
 	
-	
+	//견적
 	@GetMapping("/estimate")
 	public String EstimateList() {
 		return "open/estimate";
 	}
 	
+	//주문
 	@GetMapping("/order")
-	public String OrderList() {
+	public String Order() {
 		return "open/order";
 	}
+	
+	//주문마감
+	@GetMapping("/orderCan")
+	public String OrderCan() {
+		return "open/orderCan";
+	}
+	
+	//출고요청1
+	@GetMapping("/deliveryQ")
+	public String deliveryQ() {
+		return "open/deliveryQ";
+	}
+	
+	//출고등록2
+	@GetMapping("/deliveryG")
+	public String deliveryG() {
+		return "open/deliveryG";
+	}
+	
+	//출고처리3
+	@GetMapping("/deliveryPro")
+	public String deliveryPro() {
+		return "open/deliveryPro";
+	}
+	
+	//재고/bom현황
+	@GetMapping("/proBom")
+	public String proBom() {
+		return "open/proBom";
+	}
+
+	//배송정보
+	@GetMapping("/deliveryInfo")
+	public String deliveryInfo() {
+		return "open/deliveryInfo";
+	}
+	
 	
 	
 	
