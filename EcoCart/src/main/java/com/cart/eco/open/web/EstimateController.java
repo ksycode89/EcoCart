f04a694774ab8b553ab1db64006e21dd01c24721
kim <lldarkunitll@gmail.com>
2022-11-17 18:01:08 +0900

    autowired add

--- EcoCart/src/main/java/com/cart/eco/open/web/EstimateController.java
+++ EcoCart/src/main/java/com/cart/eco/open/web/EstimateController.java
@@ -18,10 +18,7 @@ public class EstimateController {
 	
 	@Autowired
 	EstimateService estimateService;
-	
-	@Autowired
-	OrdersService oerderService;
-	
+
 	
 	//견적 페이지 보여주는거 (+전체조회)
 	@GetMapping("/estimate")
