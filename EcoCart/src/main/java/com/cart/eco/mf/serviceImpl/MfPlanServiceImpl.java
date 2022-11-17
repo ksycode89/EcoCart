0bc371da6b94bd3ebb12e1ea81c828a85e54271d
ramier999 <ramier999@gmail.com>
2022-11-17 19:25:46 +0900

    mf modify

---
+++ EcoCart/src/main/java/com/cart/eco/mf/serviceImpl/MfPlanServiceImpl.java
@@ -0,0 +1,38 @@
+package com.cart.eco.mf.serviceImpl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cart.eco.mf.mapper.MfPlanMapper;
+import com.cart.eco.mf.service.MfPlanService;
+import com.cart.eco.mf.service.MfPlanVO;
+
+@Service
+public class MfPlanServiceImpl implements MfPlanService {
+
+	@Autowired
+	MfPlanMapper mapper;
+	
+	@Override
+	public List<MfPlanVO> getMfPlanList() {
+		return mapper.getMfPlanList();
+	}
+
+	@Override
+	public List<MfPlanVO> getOrderList(MfPlanVO mfvo) {
+		return mapper.getOrderList(mfvo);
+	}
+
+	@Override
+	public int insertMfPlan(MfPlanVO mfvo) {
+		return mapper.insertMfPlan(mfvo);
+	}
+
+	@Override
+	public int deleteMfPlan(MfPlanVO mfvo) {
+		return mapper.deleteMfPlan(mfvo);
+	}
+
+}
