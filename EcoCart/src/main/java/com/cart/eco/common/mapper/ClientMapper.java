18d9f3f69bf7f80d84608998deb87ed4329f112d
gimsang-yeong <gimsang-yeong@218.38.137.27>
2022-11-29 12:18:10 +0900

    client

--- EcoCart/src/main/java/com/cart/eco/common/mapper/ClientMapper.java
+++ EcoCart/src/main/java/com/cart/eco/common/mapper/ClientMapper.java
@@ -7,4 +7,9 @@ import com.cart.eco.common.service.ClientVO;
 public interface ClientMapper {
 	//거래처 조회
 	public List<ClientVO> clientList(ClientVO vo);
+	//거래처 등록
+	public int insertClinet(ClientVO vo);
+	//거래처 삭제
+	public int deleteClient(ClientVO vo);
+	
 }
--- EcoCart/src/main/java/com/cart/eco/common/serviceImpl/ClientServiceImpl.java
+++ EcoCart/src/main/java/com/cart/eco/common/serviceImpl/ClientServiceImpl.java
@@ -12,12 +12,44 @@ import com.cart.eco.common.service.ClientVO;
 public class ClientServiceImpl implements ClientService {
 
 	@Autowired ClientMapper Cmapper;
-	
+	//거래처 조회 
 	@Override
 	public List<ClientVO> clientList(ClientVO vo) {
-	
-	
 		return Cmapper.clientList(vo);
+	}
+	//거래처 등록 
+	@Override
+	public String insertClinet(List<ClientVO> vo) {
+		int result = 0;
+		String mes ;
+		for(ClientVO one : vo) {
+			
+			result += Cmapper.insertClinet(one);
+		}
+		if(result == vo.size()) {
+			mes = result+"등록되었습니다.";
+		}else {
+			mes = "입력에 실패했습니다.("+result + "/"+vo.size()+")";
+		}
+		
+		return mes;
+	}
+	@Override 
+	public String deleteClient(List<ClientVO> vo) {
+		
+		int result = 0;
+		String mes ;
+		for(ClientVO one : vo) {
+			
+			result += Cmapper.deleteClient(one);
+		}
+		if(result == vo.size()) {
+			mes = result+"삭제되었습니다.";
+		}else {
+			mes = "삭제에 실패했습니다.("+result + "/"+vo.size()+")";
+		}
+		
+		return mes;
 	}
 
 }
